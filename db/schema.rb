5c3883542297051c240b6d8fbb9e1040f713afa4
AlexanderHenry <alexanderhenry21@gmail.com>
2016-10-23 12:50:06 -0500

    User must sign in to Dropbox if not signed in. If signed in can access Inboxes page. Also added header/footer to HTML template.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023034628) do
+ActiveRecord::Schema.define(version: 20161022043352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,15 +24,23 @@ ActiveRecord::Schema.define(version: 20161023034628) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "tokens", force: :cascade do |t|
-    t.string   "service"
-    t.string   "token"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "models", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id", using: :btree
+  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -54,5 +62,4 @@ ActiveRecord::Schema.define(version: 20161023034628) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "tokens", "users"
 end
