c5d26546ad2cd5e14ffeb311d78794d935e93d7a
Jessica <jtstodola@gmail.com>
2016-10-23 15:26:44 -0500

    Fixed multiple file load

--- db/schema.rb
+++ db/schema.rb
@@ -29,24 +29,6 @@ ActiveRecord::Schema.define(version: 20161023182519) do
 
   add_index "inboxes", ["user_id"], name: "index_inboxes_on_user_id", using: :btree
 
-  create_table "models", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
-
   create_table "tokens", force: :cascade do |t|
     t.string   "service"
     t.string   "token"
